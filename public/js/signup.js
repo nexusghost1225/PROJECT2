ef209891af6e6e9ac043e3710c2826264f827f0c
nexusghost1225 <JGBxmas2000@hotmail.com>
2023-01-10 22:01:37 -0500

    added styles images and javascript

---
+++ public/js/signup.js
@@ -0,0 +1,57 @@
+const cloudName = "dk3t9jg7w";
+const uploadPreset = "ll0omhax";
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const user_name = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const city = document.querySelector('#city-signup').value.trim();
+  const bio = document.querySelector('#bio-signup').value.trim();
+  const profile_image = document.querySelector('#uploadedimage').src;
+
+  if (user_name && email && password && city && bio && profile_image) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ user_name, email, password, city, bio, profile_image }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+const myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: cloudName,
+    uploadPreset: uploadPreset,
+    folder: 'BF',
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the image info: ", result.info);
+      document
+        .getElementById("uploadedimage")
+        .setAttribute("src", result.info.secure_url);
+    }
+  }
+);
+
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function () {
+    myWidget.open();
+  },
+  false
+);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+
